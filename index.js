36292cfd5ab636fc1ca5c824a0ae7e7d16540a01
Cory Crook <cory@akira.md>
2019-04-17 14:41:39 -0400

    init npm and create module

---
+++ index.js
@@ -0,0 +1,15 @@
+class Singleton {
+  static _instance
+
+  static instance() {
+    if (!this._instance) {
+      this._instance = new Singleton()
+    }
+
+    return this._instance
+  }
+}
+
+module.exports = {
+  Singleton: Singleton
+}
